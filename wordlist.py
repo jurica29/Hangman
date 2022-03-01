fbe2769aadb67d1f416d929b4bf91b7f4515b52a
jurica29 <jrcpuljiz@gmail.com>
2022-03-01 21:03:40 +0000

    Add list of words and graphics as 2 files

---
+++ wordlist.py
@@ -0,0 +1,19 @@
+"""
+List of words used in the game
+"""
+dictionary = [
+    "BEAN",
+    "PEAR",
+    "KALE",
+    "KIWI",
+    "LEEK",
+    "LIME",
+    "PLUM",
+    "CORN",
+    "DATE",
+    "COLE",
+    "BEET",
+    "PEPO",
+    "MAST",
+    "OKRA",
+]
